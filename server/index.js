427ce9f370cf731420ad6cb9a508e8ad77ece79f
devdivdev <abdel.hamdani213@gmail.com>
2019-03-14 22:45:30 +0100

    Removed extra consoles

--- server/index.js
+++ server/index.js
@@ -1,26 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const fs = require('fs');
+const filename = 'hosts.txt';
 
-const helpers = require('./helpers/helpers');
+let isGif = filename => {
+    return (
+        filename.split('.').pop() === 'GIF' ||
+        filename.split('.').pop() === 'gif'
+    );
+};
+let grabFileData = data => {
+    /// Data example
+    // burger.letters.com - - [01/Jul/1995:00:00:11 -0400] "GET /shuttle/countdown/liftoff.html HTTP/1.0" 304 0
+    //1. Grab between quotes to get filename from request field
+    let fileData = {
+        filename: '',
+        status: ''
+    };
+    const regexp = /"(.*?[^\\])"/;
+    const filename = data
+        .match(regexp)[0]
+        .replace('"GET ', '')
+        .replace(' HTTP/1.0"', '')
+        .split('/')
+        .pop();
+    //2. Grab request status
+    const d = data.split(' ');
+    const dL = d.length;
+    const status = d[d.length - 2];
+    return { ...fileData, filename, status };
+};
+const lines = fs
+    .readFileSync(filename)
+    .toString()
+    .split('\n');
 
-const app = express();
-app.use(cors());
-
-app.use('/api/meal_planner', (req, res, next) => {
-    axios({
-        url: 'https://foodvisor.io/itw/food/list/?foo=bar',
-        headers: { Authorization: 'Bearer iwn-31@!3pf(w]pmarewj236^in' }
-    }).then(results => {
-        req.foodVisorData = helpers.mealPlanner(results.data);
-
-        next();
-    });
-});
-app.get('/api/meal_planner', function(req, res) {
-    res.json(req.foodVisorData);
-});
-
-app.listen(3001, function() {
-    console.log('FoodVisor app listening on port 3001!');
+const f = lines.forEach((line, index) => {
+    const filedata = grabFileData(line);
+    if (filedata.status === '200' && isGif(filedata.filename)) {
+        fs.appendFile('gifs_' + filename, filedata.filename + '\n', err => {
+            if (err) throw err;
+        });
+    }
 });
